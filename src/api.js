816dc93e2e10a9c5fb205b46910619c521ddea6c
Saad <skhatimiti@gmail.com>
2024-02-04 01:08:39 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "6e398d80b6mshd1b81b985148ba7p18e707jsn2608c5ad78fc",// enter your rapid api key here
+    "X-RapidAPI-Key": "",// enter your rapid api key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "ff635428d59c4b2ed9fea3e89c8a5cc9"; // enter your key from openweather API
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
